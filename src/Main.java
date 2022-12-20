b09d58543ea6bd93ef9e991b6b5066b1ac670928
Danil Kovalev <dannykvlv@gmail.com>
2022-12-20 15:58:06 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 10000;
-        int start_amount = 0;
-        int count_mile = 0;
+        int startAmount = 0;
+        int countMile = 0;
 
-        count_mile = amount / 20;
-        System.out.println("Количество начисленных миль за купленный билет:" + count_mile);
+        countMile = amount / 20;
+        System.out.println("Количество начисленных миль за купленный билет:" + countMile);
 
-        {
-            count_mile = count_mile + 1;
-        }
     }
 }
